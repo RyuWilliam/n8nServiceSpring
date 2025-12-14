0ce939b09741f5fbdadcb3e8af71f0e7d6c6fd3a
William González <ruyk1120@gmail.com>
2025-12-14 02:07:32 -0500

    cambios en el modelo

--- src/main/java/co/yuno/n8nService/persistence/entity/Procesed.java
+++ src/main/java/co/yuno/n8nService/persistence/entity/Procesed.java
@@ -11,16 +11,24 @@ public class Procesed {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idProcesed;
 
-
     @Column(length = 2000)
     private String summary;
 
     @Column(length = 100)
+    @Enumerated(EnumType.STRING)
     private Phase phase;
 
     private String projectName;
 
-    private String detailType;
+    private String payment;
+
+    private String risk;
+
+    private String legal;
+
+    private String Technical;
+
+    private String commercial;
 
     private String detail;
 
@@ -56,12 +64,44 @@ public class Procesed {
         this.projectName = projectName;
     }
 
-    public String getDetailType() {
-        return detailType;
+    public String getPayment() {
+        return payment;
     }
 
-    public void setDetailType(String detailType) {
-        this.detailType = detailType;
+    public void setPayment(String payment) {
+        this.payment = payment;
+    }
+
+    public String getRisk() {
+        return risk;
+    }
+
+    public void setRisk(String risk) {
+        this.risk = risk;
+    }
+
+    public String getLegal() {
+        return legal;
+    }
+
+    public void setLegal(String legal) {
+        this.legal = legal;
+    }
+
+    public String getTechnical() {
+        return Technical;
+    }
+
+    public void setTechnical(String technical) {
+        Technical = technical;
+    }
+
+    public String getCommercial() {
+        return commercial;
+    }
+
+    public void setCommercial(String commercial) {
+        this.commercial = commercial;
     }
 
     public String getDetail() {
--- src/main/java/co/yuno/n8nService/persistence/repository/ProcesedRepository.java
+++ src/main/java/co/yuno/n8nService/persistence/repository/ProcesedRepository.java
@@ -3,5 +3,9 @@ package co.yuno.n8nService.persistence.repository;
 import co.yuno.n8nService.persistence.entity.Procesed;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProcesedRepository extends JpaRepository<Procesed, Integer> {
+
+    List<Procesed> findByProjectNameAndIdProcesedLessThan(String projectName, Integer idProcesed);
 }
